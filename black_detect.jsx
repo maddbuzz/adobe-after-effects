79930c844398aecae1be853eb5ab3295f82d1a64
maddbuzz <maddbuzz@gmail.com>
2026-01-03 01:51:24 +0800

    fix

--- black_detect.jsx
+++ black_detect.jsx
@@ -1,4 +1,4 @@
-const FFMPEG_PATH = "E:\\_Downloads_\\ffmpeg-2025-12-10-git-4f947880bd-full_build\\bin\\ffmpeg.exe";
+var FFMPEG_PATH = "E:\\_Downloads_\\ffmpeg-2025-12-10-git-4f947880bd-full_build\\bin\\ffmpeg.exe";
 
 var comp = app.project.activeItem;
 if (!(comp instanceof CompItem)) {
@@ -14,26 +14,26 @@ app.beginUndoGroup("Black markers");
 
 for (var i = 0; i < selectedLayers.length; i++) {
   var layer = selectedLayers[i];
-  
+
   // Проверяем, что слой имеет источник файла
   if (!(layer.source instanceof FootageItem)) {
     continue;
   }
-  
+
   var footageItem = layer.source;
   if (!footageItem || !footageItem.file) {
     continue;
   }
-  
+
   var video_file = footageItem.file;
-  
+
   // Проверяем существование файла
   video_file = new File(video_file.fsName);
   if (!video_file.exists) {
     alert("Файл не найден: " + video_file.fsName + "\nСлой: " + layer.name);
     continue;
   }
-  
+
   // Получаем пути к файлам
   var video_path = video_file.fsName;
   // Получаем имя файла без расширения для имени лог-файла
@@ -41,6 +41,11 @@ for (var i = 0; i < selectedLayers.length; i++) {
   var video_name_no_ext = video_name.substring(0, video_name.lastIndexOf(".")) || video_name;
   var log_file = new File(video_file.parent.fsName + "\\" + video_name_no_ext + ".ffmpeg.log");
   var log_file_path = log_file.fsName;
+
+  // Экранируем пути для использования в PowerShell команде
+  var ffmpeg_path_escaped = FFMPEG_PATH.replace(/'/g, "''");
+  var video_path_escaped = video_path.replace(/'/g, "''");
+  var log_file_path_escaped = log_file_path.replace(/'/g, "''");
 
   // Создаём bat-файл, который запускает PowerShell команду
   // Это позволяет правильно обработать пути с кириллицей и перенаправление вывода
@@ -50,18 +55,18 @@ for (var i = 0; i < selectedLayers.length; i++) {
   temp_bat.write("@chcp 65001 >nul\n");
   temp_bat.write("@echo off\n");
   // Запускаем PowerShell команду с правильным перенаправлением stderr
-  temp_bat.write("powershell -NoProfile -Command \"& '" + FFMPEG_PATH.replace(/'/g, "''") + "' -i '" + video_path.replace(/'/g, "''") + "'" +
+  temp_bat.write("powershell -NoProfile -Command \"& '" + ffmpeg_path_escaped + "' -i '" + video_path_escaped + "'" +
     " -vf blackdetect=d=0:pic_th=0.98:pix_th=0.10 -an -f null - *>&1 | " +
-    "Out-File -FilePath '" + log_file_path.replace(/'/g, "''") + "' -Encoding UTF8\"\n");
+    "Out-File -FilePath '" + log_file_path_escaped + "' -Encoding UTF8\"\n");
   temp_bat.close();
-  
+
   system.callSystem("cmd /c \"" + temp_bat.fsName + "\"");
-  
+
   // Удаляем временный bat-файл
   if (temp_bat.exists) {
     temp_bat.remove();
   }
-  
+
   // Небольшая задержка для записи файла на диск
   $.sleep(1000);
 
@@ -71,7 +76,7 @@ for (var i = 0; i < selectedLayers.length; i++) {
     alert("Лог-файл не создан: " + log_file_path);
     continue;
   }
-  
+
   // Проверяем размер файла
   if (log_file.length === 0) {
     alert("Лог-файл пустой: " + log_file_path);
@@ -88,29 +93,26 @@ for (var i = 0; i < selectedLayers.length; i++) {
   while ((match = regex.exec(text)) !== null) {
     var startTime = parseFloat(match[1]);
     var endTime = parseFloat(match[2]);
-    
+
     // Устанавливаем маркеры на слое
-    // В After Effects маркеры слоя доступны через markerProperty
     try {
-      // Используем markerProperty напрямую - он должен быть доступен для всех слоев
-      layer.markerProperty.setValueAtTime(startTime, new MarkerValue("BLACK START"));
-      layer.markerProperty.setValueAtTime(endTime, new MarkerValue("BLACK END"));
-    } catch (e) {
-      // Если markerProperty недоступен, пробуем альтернативный способ
-      try {
-        var markerProp = layer.property("Marker");
-        if (markerProp) {
-          markerProp.setValueAtTime(startTime, new MarkerValue("BLACK START"));
-          markerProp.setValueAtTime(endTime, new MarkerValue("BLACK END"));
-        } else {
-          alert("Не удалось установить маркеры на слое: " + layer.name + "\nОшибка: " + e.toString());
-          break;
-        }
-      } catch (e2) {
-        alert("Ошибка при установке маркера на слое " + layer.name + " в момент " + startTime + ":\n" + e.toString() + "\n" + e2.toString());
+      var markerProp = layer.property("Marker");
+      if (markerProp) {
+        markerProp.setValueAtTime(startTime, new MarkerValue("BLACK START"));
+        markerProp.setValueAtTime(endTime, new MarkerValue("BLACK END"));
+      } else {
+        alert("Не удалось установить маркеры на слое: " + layer.name + "\nОшибка: " + e.toString());
         break;
       }
+    } catch (e2) {
+      alert("Ошибка при установке маркера на слое " + layer.name + " в момент " + startTime + ":\n" + e.toString() + "\n" + e2.toString());
+      break;
     }
+  }
+
+  // Удаляем лог-файл после обработки
+  if (log_file.exists) {
+    log_file.remove();
   }
 }
 
