37ddd1d55fa384ab68afbc814b94fc12d769c239
maddbuzz <maddbuzz@gmail.com>
2026-01-03 02:44:22 +0800

    1

--- black_detect.jsx
+++ black_detect.jsx
@@ -34,6 +34,14 @@ for (var i = 0; i < selectedLayers.length; i++) {
     continue;
   }
 
+  // Вычисляем границы видимой части слоя относительно начала файла
+  // layer.startTime - время начала источника относительно начала композиции
+  // layer.inPoint - время начала видимой части слоя в композиции
+  // layer.outPoint - время конца видимой части слоя в композиции
+  var visibleStartInFile = layer.inPoint - layer.startTime;  // Начало видимой части относительно файла
+  var visibleEndInFile = layer.outPoint - layer.startTime;   // Конец видимой части относительно файла
+  var layerDuration = layer.outPoint - layer.inPoint;        // Длительность видимой части
+
   // Получаем пути к файлам
   var video_path = video_file.fsName;
   // Получаем имя файла без расширения для имени лог-файла
@@ -47,6 +55,12 @@ for (var i = 0; i < selectedLayers.length; i++) {
   var video_path_escaped = video_path.replace(/'/g, "''");
   var log_file_path_escaped = log_file_path.replace(/'/g, "''");
 
+  // Формируем параметры для обработки только видимой части видео
+  // -ss указывает начальную точку, -t указывает длительность
+  // Всегда добавляем параметры для ускорения работы (даже если слой не обрезан, это не повредит)
+  var ffmpeg_ss_param = " -ss " + visibleStartInFile.toFixed(6);
+  var ffmpeg_t_param = " -t " + layerDuration.toFixed(6);
+
   // Создаём bat-файл, который запускает PowerShell команду
   // Это позволяет правильно обработать пути с кириллицей и перенаправление вывода
   var temp_bat = new File(Folder.temp.fsName + "\\black_detect_" + video_name_no_ext + "_" + i + ".bat");
@@ -56,7 +70,8 @@ for (var i = 0; i < selectedLayers.length; i++) {
   temp_bat.write("@echo off\n");
   // Запускаем PowerShell команду с правильным перенаправлением stderr
   // Выводим в консоль и одновременно записываем в файл
-  temp_bat.write("powershell -NoProfile -Command \"& '" + ffmpeg_path_escaped + "' -i '" + video_path_escaped + "'" +
+  // Обрабатываем только видимую часть видео для ускорения работы
+  temp_bat.write("powershell -NoProfile -Command \"& '" + ffmpeg_path_escaped + "'" + ffmpeg_ss_param + " -i '" + video_path_escaped + "'" + ffmpeg_t_param +
     " -vf blackdetect=d=0:pic_th=0.98:pix_th=0.10 -an -f null - *>&1 | " +
     "ForEach-Object { Write-Host $_; $_ } | Out-File -FilePath '" + log_file_path_escaped + "' -Encoding UTF8\"\n");
   temp_bat.close();
@@ -91,24 +106,52 @@ for (var i = 0; i < selectedLayers.length; i++) {
   var regex = /black_start:([\d.]+)\s+black_end:([\d.]+)/g;
   var match;
 
+  // Время из blackdetect при использовании -ss перед -i выводится относительно начала обрабатываемого сегмента
+  // То есть относительно начала видимой части, а не относительно начала файла
+  // Маркеры на слое в After Effects устанавливаются относительно времени слоя (где 0 = layer.startTime)
+  // Видимая часть начинается на времени visibleStartInFile относительно начала слоя
+  // Поэтому время маркера на слое = время из blackdetect + visibleStartInFile
+  var markersFound = 0;
+  var markersSet = 0;
+  
   while ((match = regex.exec(text)) !== null) {
-    var startTime = parseFloat(match[1]);
-    var endTime = parseFloat(match[2]);
+    markersFound++;
+    var startTime = parseFloat(match[1]);  // Время относительно начала видимой части (из-за -ss)
+    var endTime = parseFloat(match[2]);    // Время относительно начала видимой части (из-за -ss)
 
-    // Устанавливаем маркеры на слое
-    try {
-      var markerProp = layer.property("Marker");
-      if (markerProp) {
-        markerProp.setValueAtTime(startTime, new MarkerValue("BLACK START"));
-        markerProp.setValueAtTime(endTime, new MarkerValue("BLACK END"));
-      } else {
-        alert("Не удалось установить маркеры на слое: " + layer.name + "\nОшибка: " + e.toString());
+    // Проверяем, что маркеры попадают в диапазон видимой части
+    if (startTime >= 0 && startTime <= layerDuration && endTime >= 0 && endTime <= layerDuration && endTime > startTime) {
+      // Преобразуем время в время относительно начала слоя
+      // Время маркера на слое = время относительно видимой части + смещение начала видимой части
+      var markerStartTime = startTime + visibleStartInFile;
+      var markerEndTime = endTime + visibleStartInFile;
+      
+      try {
+        var markerProp = layer.property("Marker");
+        if (markerProp) {
+          // Маркеры устанавливаются относительно времени слоя (где 0 = layer.startTime)
+          markerProp.setValueAtTime(markerStartTime, new MarkerValue("BLACK START"));
+          markerProp.setValueAtTime(markerEndTime, new MarkerValue("BLACK END"));
+          markersSet++;
+        } else {
+          alert("Не удалось установить маркеры на слое: " + layer.name);
+          break;
+        }
+      } catch (e2) {
+        alert("Ошибка при установке маркера на слое " + layer.name + " в момент " + markerStartTime + ":\n" + e2.toString());
         break;
       }
-    } catch (e2) {
-      alert("Ошибка при установке маркера на слое " + layer.name + " в момент " + startTime + ":\n" + e.toString() + "\n" + e2.toString());
-      break;
     }
+  }
+  
+  // Отладочный вывод через alert
+  if (markersFound > 0) {
+    alert("Слой: " + layer.name + "\n" +
+          "Найдено маркеров: " + markersFound + "\n" +
+          "Установлено маркеров: " + markersSet + "\n" +
+          "startTime: " + layer.startTime + ", inPoint: " + layer.inPoint + ", outPoint: " + layer.outPoint + "\n" +
+          "visibleStartInFile: " + visibleStartInFile + ", visibleEndInFile: " + visibleEndInFile + "\n" +
+          "layerDuration: " + layerDuration);
   }
 
   // Удаляем лог-файл после обработки
